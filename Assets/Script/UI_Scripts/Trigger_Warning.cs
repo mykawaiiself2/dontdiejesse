033c5b1d6cb09e9799781f5ad80c1e59fafdf47e
Michelle <j.s.frankfort@gmail.com>
2023-06-11 13:53:46 +0200

    Added post-processing

--- Assets/Script/UI_Scripts/Trigger_Warning.cs
+++ Assets/Script/UI_Scripts/Trigger_Warning.cs
@@ -9,13 +9,20 @@ public class Trigger_Warning : MonoBehaviour
     [SerializeField] private float _maxTime = 5;
     [SerializeField] private bool _warningActive;
     [SerializeField] private bool _mouseIsMoving = false;
+    public GameObject warningElement;
+    public Button mybutton;
+
+    // ui prompts for the player
     private bool a_bool = true;
     private bool s_bool = true;
     private bool w_bool = true;
     private bool d_bool = true;
-    public GameObject Tutorail, w_visaul, a_visaul, s_visaul, d_visaul;
-    public GameObject warningElement;
-    public Button mybutton;
+    public GameObject w_visaul, a_visaul, s_visaul, d_visaul;
+    public GameObject movement_text;
+    public GameObject mouseText;
+    private float mouseTimer;
+    private bool myMouseUI = true;
+
 
 
     // Start is called before the first frame update
@@ -37,6 +44,7 @@ public class Trigger_Warning : MonoBehaviour
         if (_timer >= _maxTime)
         {
             warningElement.SetActive(true);
+            DeactivedVisaulPrompts();
             Time.timeScale = 0;
             _timer = 0;
             _warningActive = true;
@@ -111,6 +119,36 @@ public class Trigger_Warning : MonoBehaviour
             w_visaul.SetActive(false);
             w_bool = false;
         }
+        if(!a_bool && !d_bool && !w_bool && !s_bool)
+        {
+            movement_text.SetActive(false);
+        }
+        if (Input.GetAxis("Mouse X") != 0 && Input.GetAxis("Mouse Y") != 0 && myMouseUI)
+        {
+            mouseTimer += Time.deltaTime;
+        }
+        if (mouseTimer >= 0.5f)
+        {
+            mouseText.SetActive(false);
+            myMouseUI = false;
+            mouseTimer = 0f;
+        }
+
+    }
+
+    private void DeactivedVisaulPrompts()
+    {
+        a_bool = false;
+        a_visaul.SetActive(false);
+        d_bool = false;
+        d_visaul.SetActive(false);
+        s_bool = false;
+        s_visaul.SetActive(false);
+        w_bool = false;
+        w_visaul.SetActive(false);
+        movement_text.SetActive(false);
+        myMouseUI = false;
+        mouseText.SetActive(false);
     }
 
     private void ActiveVisaulButtonPrompt()
@@ -123,5 +161,8 @@ public class Trigger_Warning : MonoBehaviour
         s_visaul.SetActive(true);
         w_bool = true;
         w_visaul.SetActive(true);
+        movement_text.SetActive(true);
+        myMouseUI = true;
+        mouseText.SetActive(true);
     }
 }
